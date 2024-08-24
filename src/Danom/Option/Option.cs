7bf350147f44ae67568e2df943e5188d2bc0851d
Pim Brouwers <pimnation@gmail.com>
2024-08-23 20:29:12 -0400

    option code comments

--- src/Danom/Option/Option.cs
+++ src/Danom/Option/Option.cs
@@ -1,9 +1,8 @@
 namespace Danom;
 
 /// <summary>
-/// Represents when an actual value might not exist for a value or named
-/// variable. An option has an underlying type and can hold a value of that
-/// type, or it might not have a value.
+/// Represents the existence, or not, of a value. Provides greater safety than
+/// using nulls, and allows for more expressive code with exhaustive matching.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public interface IOption<T>
@@ -19,47 +18,130 @@ public interface IOption<T>
     IOption<T> OrElseWith(Func<IOption<T>> ifNoneWith);
 }
 
-/// <inheritdoc />
+/// <inheritdoc cref="IOption{T}" />
 public sealed class Option<T>
-    : Choice<T, Unit>, IOption<T>
+    : Maybe<T, Unit>, IOption<T>
 {
     internal Option(T t) : base(t) { }
 
     internal Option() : base(Unit.Value) { }
 
+    /// <summary>
+    /// Returns true if the Option is Some, false otherwise.
+    /// </summary>
     public bool IsSome => _isT1;
+
+    /// <summary>
+    /// Returns true if the Option is None, false otherwise.
+    /// </summary>
     public bool IsNone => _isT2;
 
+    /// <summary>
+    /// Creates a new Option with the specified value.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public static IOption<T> Some(T value) =>
         new Option<T>(value);
 
+    /// <summary>
+    /// Creates a new Option with the specified value wrapped in a completed
+    /// Task.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public static Task<IOption<T>> SomeAsync(T value) =>
         Task.FromResult(Some(value));
 
-    public static async Task<IOption<T>> SomeAsync(Task<T> valueTask) =>
-        Some(await valueTask);
+    /// <summary>
+    /// Creates a new Option with the value of the awaited Task.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static async Task<IOption<T>> SomeAsync(Task<T> value) =>
+        Some(await value);
 
+    /// <summary>
+    /// Creates a new Option with no value.
+    /// </summary>
+    /// <returns></returns>
     public static IOption<T> None() =>
         new Option<T>();
 
+    /// <summary>
+    /// Creates a new Option with no value wrapped in a completed Task.
+    /// </summary>
+    /// <returns></returns>
     public static Task<IOption<T>> NoneAsync() =>
         Task.FromResult(None());
 
+    /// <summary>
+    /// Evaluates the some delegate if the Option is Some, otherwise evaluates
+    /// the none delegate.
+    /// </summary>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="some"></param>
+    /// <param name="none"></param>
+    /// <returns></returns>
     public U Match<U>(Func<T, U> some, Func<U> none) =>
         Match(t1: some, t2: _ => none());
 
+    /// <summary>
+    /// Evaluates the bind delegate if the Option is Some, otherwise returns
+    /// None.
+    /// </summary>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="bind"></param>
+    /// <returns></returns>
     public IOption<U> Bind<U>(
         Func<T, IOption<U>> bind) =>
         Match(bind, Option<U>.None);
 
+    /// <summary>
+    /// Evaluates the map delegate if the Option is Some, otherwise returns
+    /// None.
+    /// </summary>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="map"></param>
+    /// <returns></returns>
     public IOption<U> Map<U>(
         Func<T, U> map) =>
         Bind(x => Option<U>.Some(map(x)));
 
+    /// <summary>
+    /// Returns the value of the Option if it is T, otherwise returns the
+    /// specified default value.
+    /// </summary>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    public T DefaultValue(
+         T defaultValue) =>
+         Match(some => some, _ => defaultValue);
+
+    /// <summary>
+    /// Returns the value of the Option if it is T, otherwise returns the
+    /// </summary>
+    /// <param name="defaultWith"></param>
+    /// <returns></returns>
+    public T DefaultWith(
+        Func<T> defaultWith) =>
+        Match(some => some, _ => defaultWith());
+
+    /// <summary>
+    /// Returns the value of the Option if it is Some, otherwise returns the
+    /// specified ifNone value.
+    /// </summary>
+    /// <param name="ifNone"></param>
+    /// <returns></returns>
     public IOption<T> OrElse(
         IOption<T> ifNone) =>
         Match(Option<T>.Some, () => ifNone);
 
+    /// <summary>
+    /// Returns the value of the Option if it is Some, otherwise returns the
+    /// </summary>
+    /// <param name="ifNoneWith"></param>
+    /// <returns></returns>
     public IOption<T> OrElseWith(
         Func<IOption<T>> ifNoneWith) =>
         Match(Option<T>.Some, ifNoneWith);
@@ -70,20 +152,19 @@ public sealed class Option<T>
             none: () => "None");
 }
 
-public static class Option
-{
-    public static IOption<T> Some<T>(T value) =>
-         new Option<T>(value);
-
-    public static Task<IOption<T>> SomeAsync<T>(T value) =>
-        Task.FromResult(Some(value));
-
-    public static async Task<IOption<T>> SomeAsync<T>(Task<T> valueTask) =>
-        Some(await valueTask);
-}
-
+/// <summary>
+/// Extension methods to allow Option matching using
+/// </summary>
 public static class OptionActionExtensions
 {
+    /// <summary>
+    /// If the Option is Some, evaluates the some delegate, otherwise evaluates
+    /// the none delegate.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="option"></param>
+    /// <param name="some"></param>
+    /// <param name="none"></param>
     public static void Match<T>(this IOption<T> option, Action<T> some, Action none)
     {
         if (option.ToNullable() is T t)
