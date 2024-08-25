ef45583d1f542aec04fefa0956bf52a9872b568d
Pim Brouwers <pimnation@gmail.com>
2024-08-24 20:50:47 -0400

    unit tests

--- test/Danom.Tests/UnitTests.cs
+++ test/Danom.Tests/UnitTests.cs
@@ -15,4 +15,32 @@ public sealed class UnitTests
     {
         Assert.Equal("()", Unit.Value.ToString());
     }
+
+    [Fact]
+    public void GetHashCodeShouldBeZero()
+    {
+        Assert.Equal(0, Unit.Value.GetHashCode());
+    }
+
+    [Fact]
+    public void CompareToShouldBeZero()
+    {
+        Assert.Equal(0, Unit.Value.CompareTo(Unit.Value));
+    }
+
+    [Fact]
+    public void EqualityOperatorShouldWork()
+    {
+#pragma warning disable CS1718 // Comparison made to same variable
+        Assert.True(Unit.Value == Unit.Value);
+#pragma warning restore CS1718 // Comparison made to same variable
+    }
+
+    [Fact]
+    public void InequalityOperatorShouldWork()
+    {
+#pragma warning disable CS1718 // Comparison made to same variable
+        Assert.False(Unit.Value != Unit.Value);
+#pragma warning restore CS1718 // Comparison made to same variable
+    }
 }
