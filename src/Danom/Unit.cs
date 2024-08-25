b632408767b16490259ba7f80313ff4d0e0c206e
Pim Brouwers <pimnation@gmail.com>
2024-08-24 20:49:13 -0400

    unit formatting

--- src/Danom/Unit.cs
+++ src/Danom/Unit.cs
@@ -10,13 +10,27 @@ public readonly struct Unit
 {
     public static readonly Unit Value;
     public static readonly Task<Unit> ValueAsync = Task.FromResult(Value);
-    public override readonly bool Equals(object? obj) => obj is Unit;
-    public override readonly int GetHashCode() => 0;
-    public static bool operator ==(Unit left, Unit right) => left.Equals(right);
-    public static bool operator !=(Unit left, Unit right) => !(left == right);
-    public readonly bool Equals(Unit other) => true;
-    public override readonly string ToString() => "()";
-    public int CompareTo(Unit other) => 0;
+
+    public static bool operator ==(Unit left, Unit right) =>
+        left.Equals(right);
+
+    public static bool operator !=(Unit left, Unit right) =>
+        !(left == right);
+
+    public readonly bool Equals(Unit other) =>
+        true;
+
+    public int CompareTo(Unit other) =>
+        0;
+
+    public override readonly string ToString() =>
+        "()";
+
+    public override readonly bool Equals(object? obj) =>
+        obj is Unit;
+
+    public override readonly int GetHashCode() =>
+        0;
 }
 
 /// <summary>
