1c4ec8c5070cbed488bf5891122f28c2671291d3
Pim Brouwers <pimnation@gmail.com>
2024-08-30 14:04:02 -0400

    console sample

---
+++ samples/Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Danom;
+
+public static class OptionSamples
+{
+    private static readonly IEnumerable<int> _store = Enumerable.Range(1, 100);
+
+    public static Option<int> TryFindInt(Func<int, bool> predicate) =>
+        _store.FirstOrDefault(predicate).ToOption();
+
+    public static void Test()
+    {
+        if (Option<int>.Some(1).DefaultValue(1) == 1)
+        {
+            Console.WriteLine("Some");
+        }
+    }
+}
+
+// write main method
+public static class Program
+{
+    public static void Main()
+    {
+        OptionSamples.Test();
+    }
+}
