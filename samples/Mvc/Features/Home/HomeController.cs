6a62210714385451c3b1d44d277c7b3494c2da27
Pim Brouwers <pimnation@gmail.com>
2024-10-03 09:32:39 -0400

    mvc sample improvements

--- samples/Mvc/Features/Home/HomeController.cs
+++ samples/Mvc/Features/Home/HomeController.cs
@@ -1,54 +1,9 @@
 namespace Danom.Samples.Mvc;
 
-using Danom.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
-public sealed class HomeController : DanomController
+public sealed class HomeController : Controller
 {
     public IActionResult Index() =>
         View();
-
-    public IActionResult OptionSome() =>
-        ViewOption(
-            option: Option.Some("Hello, World!"),
-            viewName: "Detail");
-
-    public IActionResult OptionNone() =>
-        ViewOption(
-            option: Option<string>.None(),
-            viewName: "Detail",
-            noneAction: () => NotFound("Not found!"));
-
-    public IActionResult ResultOk() =>
-        ViewResult(
-            result: Result.Ok("Success!"),
-            viewName: "Detail");
-
-    public IActionResult ResultError() =>
-        ViewResult(
-            result: Result<string, string>.Error("An error occurred."),
-            errorAction: errors => View("Detail", errors),
-            viewName: "Detail");
-
-    public IActionResult ResultErrors() =>
-        ViewResult(
-            result: Result<string>.Error("An error occurred."),
-            viewName: "Detail");
-
-    public IActionResult ResultOptionOk() =>
-        ViewResultOption(
-            resultOption: ResultOption.Ok("Hello, World!"),
-            viewName: "Detail");
-
-    public IActionResult ResultOptionError() =>
-        ViewResultOption(
-            resultOption: ResultOption<string, string>.Error("An error occurred."),
-            errorAction: errors => View("Detail", errors),
-            viewName: "Detail");
-
-    public IActionResult ResultOptionErrors() =>
-        ViewResultOption(
-            resultOption: ResultOption<string>.Error("An error occurred."),
-            viewName: "Detail");
-
 }
---
+++ samples/Mvc/Features/Result/ResultController.cs
@@ -0,0 +1,30 @@
+namespace Danom.Samples.Mvc;
+
+using Danom.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+public sealed class ResultController
+    : DanomController
+{
+    private readonly Result<string, ResultErrors> _okResult = Result.Ok("Success!");
+    private readonly Result<string, ResultErrors> _errorResult = Result<string>.Error(["An error occurred."]);
+    private readonly Result<string, string> _stringErrorResult = Result<string, string>.Error("An error occurred.");
+
+    public IActionResult Ok() =>
+        ViewResult(
+            result: _okResult,
+            viewName: "Detail");
+
+    public IActionResult Error() =>
+        ViewResult(
+            result: _errorResult,
+            viewName: "Detail");
+
+    // Demonstrating error customization, using a string literal for error output
+    public IActionResult ErrorCustom() =>
+        ViewResult(
+            result: _stringErrorResult,
+            errorAction: errors => View("Detail", errors),
+            viewName: "Detail");
+
+}
