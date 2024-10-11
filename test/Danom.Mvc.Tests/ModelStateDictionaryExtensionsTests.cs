49ca63776af02166d1272198c7f8cf9e3d5d3f3f
Pim Brouwers <pimnation@gmail.com>
2024-10-11 07:07:30 -0400

    danom mvc beta1

--- test/Danom.Mvc.Tests/ModelStateDictionaryExtensionsTests.cs
+++ test/Danom.Mvc.Tests/ModelStateDictionaryExtensionsTests.cs
@@ -29,6 +29,7 @@ public sealed class ModelStateDictionaryExtensionsTests
         var errors = new ResultErrors("Key1", "Error1");
         modelState.AddResultErrors(errors);
         Assert.Equal(1, modelState.ErrorCount);
+        Assert.True(modelState.ContainsKey("Key1"));
     }
 
     [Fact]
@@ -40,5 +41,7 @@ public sealed class ModelStateDictionaryExtensionsTests
             new ResultError("Key2", "Error2")]);
         modelState.AddResultErrors(errors);
         Assert.Equal(2, modelState.ErrorCount);
+        Assert.True(modelState.ContainsKey("Key1"));
+        Assert.True(modelState.ContainsKey("Key2"));
     }
 }
