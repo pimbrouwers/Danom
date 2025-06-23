58d4de5af3e9a6521eb92d9d6a1ba3edcb21a310
Pim Brouwers <pimnation@gmail.com>
2025-06-23 11:00:12 -0400

    removed type specific option tonullable conversions

--- src/Danom/Option/OptionNullable.cs
+++ src/Danom/Option/OptionNullable.cs
@@ -16,15 +16,6 @@ public static class OptionNullableExtensions
         x is not null ? Option<T>.Some(x) : Option<T>.None();
 
     /// <summary>
-    /// Converts a nullable strict to an option.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="x"></param>
-    /// <returns></returns>
-    public static Option<T> ToOption<T>(this T? x) where T : struct =>
-        (x is T t) ? Option<T>.Some(t) : Option<T>.None();
-
-    /// <summary>
     /// Converts a nullable string to an option.
     /// </summary>
     /// <param name="x"></param>
@@ -38,102 +29,30 @@ public static class OptionNullableExtensions
     /// <typeparam name="T"></typeparam>
     /// <param name="option"></param>
     /// <returns></returns>
-    public static T? ToNullable<T>(this Option<T> option) =>
+    public static T? ToNullable<T>(this Option<T> option) where T : class =>
         option.Match(some: x => x, none: () => default!);
+}
+
+/// <summary>
+/// Contains extension methods for <see cref="Option{T}"/> that allow for
+/// converting between nullable struct types and options.
+/// </summary>
+public static class OptionNullableStructExtensions
+{
+    /// <summary>
+    /// Converts a nullable strict to an option.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static Option<T> ToOption<T>(this T? x) where T : struct =>
+        (x is T t) ? Option<T>.Some(t) : Option<T>.None();
 
     /// <summary>
     /// Converts a char option to a nullable value.
     /// </summary>
     /// <param name="option"></param>
     /// <returns></returns>
-    public static char? ToNullable(this Option<char> option) =>
-        option.Match(x => new char?(x), () => null);
-
-    /// <summary>
-    /// Converts a bool option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static bool? ToNullable(this Option<bool> option) =>
-        option.Match(x => new bool?(x), () => null);
-
-    /// <summary>
-    /// Converts a byte option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static byte? ToNullable(this Option<byte> option) =>
-        option.Match(x => new byte?(x), () => null);
-
-    /// <summary>
-    /// Converts a short option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static short? ToNullable(this Option<short> option) =>
-        option.Match(x => new short?(x), () => null);
-
-    /// <summary>
-    /// Converts a ushort option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static int? ToNullable(this Option<int> option) =>
-        option.Match(x => new int?(x), () => null);
-
-    /// <summary>
-    /// Converts a uint option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>/
-    public static long? ToNullable(this Option<long> option) =>
-        option.Match(x => new long?(x), () => null);
-
-    /// <summary>
-    /// Converts a ulong option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static decimal? ToNullable(this Option<decimal> option) =>
-        option.Match(x => new decimal?(x), () => null);
-
-    /// <summary>
-    /// Converts a double option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static double? ToNullable(this Option<double> option) =>
-        option.Match(x => new double?(x), () => null);
-
-    /// <summary>
-    /// Converts a float option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static float? ToNullable(this Option<float> option) =>
-        option.Match(x => new float?(x), () => null);
-
-    /// <summary>
-    /// Converts a decimal option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static Guid? ToNullable(this Option<Guid> option) =>
-        option.Match(x => new Guid?(x), () => null);
-
-    /// <summary>
-    /// Converts a DateTime option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static DateTime? ToNullable(this Option<DateTime> option) =>
-        option.Match(x => new DateTime?(x), () => null);
-
-    /// <summary>
-    /// Converts a DateTimeOffset option to a nullable value.
-    /// </summary>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    public static DateOnly? ToNullable(this Option<DateOnly> option) =>
-        option.Match(x => new DateOnly?(x), () => null);
+    public static T? ToNullable<T>(this Option<T> option) where T : struct =>
+        option.Match(x => new T?(x), () => null);
 }
