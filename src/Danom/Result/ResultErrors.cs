bb2a3d49112ee480349864be5d5e34dbfb13c039
Pim Brouwers <pimnation@gmail.com>
2025-07-15 18:10:33 -0400

    improved resulterrors construction

--- src/Danom/Result/ResultErrors.cs
+++ src/Danom/Result/ResultErrors.cs
@@ -1,5 +1,6 @@
 namespace Danom
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
@@ -9,7 +10,20 @@ namespace Danom
     /// </summary>
     public sealed class ResultErrors : IEnumerable<ResultError>
     {
-        private readonly List<ResultError> _errors = new List<ResultError>();
+        private readonly Dictionary<string, List<string>> _resultErrors =
+            new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Gets the error collection for the specified key.
+        /// If the key does not exist, a <see cref="KeyNotFoundException"/>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException"></exception>
+        public ResultError this[string key] =>
+            _resultErrors.TryGetValue(key, out var errors)
+                ? new ResultError(key, errors.ToArray())
+                : throw new KeyNotFoundException($"No errors found for key '{key}'.");
 
         /// <summary>
         /// Creates a new empty instance of <see cref="ResultErrors"/>.
@@ -18,53 +32,12 @@ namespace Danom
 
         /// <summary>
         /// Creates a new instance of <see cref="ResultErrors"/> from the specified
-        /// errors.
+        /// key and errors.
         /// </summary>
-        /// <param name="errors"></param>/
-        public ResultErrors(ResultErrors errors)
-        {
-            if (errors != null)
-            {
-                _errors.AddRange(errors);
-            }
-        }
-
-        /// <summary>
-        /// Creates a new instance of <see cref="ResultErrors"/> from the
-        /// specified error.
-        /// </summary>
-        /// <param name="error"></param>
-        public ResultErrors(ResultError error)
-            : this()
-        {
-            if (error != null)
-            {
-                _errors.Add(error);
-            }
-        }
-
-
-        /// <summary>
-        /// Creates a new instance of <see cref="ResultErrors"/> from the specified
-        /// errors.
-        /// </summary>
+        /// <param name="key"></param>
         /// <param name="errors"></param>
-        public ResultErrors(IEnumerable<ResultError> errors)
-        {
-            if (errors != null)
-            {
-                _errors.AddRange(errors);
-            }
-        }
-
-
-        /// <summary>
-        /// Creates a new instance of <see cref="ResultErrors"/> from the specified
-        /// error.
-        /// </summary>
-        /// <param name="error"></param>
-        public ResultErrors(string error)
-            : this(new ResultError(error)) { }
+        public ResultErrors(string key, params string[] errors) =>
+            Add(key, errors);
 
         /// <summary>
         /// Creates a new instance of <see cref="ResultErrors"/> from the specified
@@ -73,16 +46,15 @@ namespace Danom
         /// <param name="key"></param>
         /// <param name="error"></param>
         public ResultErrors(string key, string error)
-            : this(new ResultError(key, error)) { }
+            : this(key, new[] { error }) { }
 
         /// <summary>
         /// Creates a new instance of <see cref="ResultErrors"/> from the specified
-        /// key and errors.
+        /// error.
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="errors"></param>
-        public ResultErrors(string key, params string[] errors)
-            : this(new ResultError(key, errors)) { }
+        /// <param name="error"></param>
+        public ResultErrors(string error)
+            : this(string.Empty, error) { }
 
         /// <summary>
         /// Creates a new instance of <see cref="ResultErrors"/> from the specified
@@ -90,21 +62,74 @@ namespace Danom
         /// </summary>
         /// <param name="errors"></param>
         public ResultErrors(params string[] errors)
-            : this(errors.Select(m => new ResultError(m))) { }
+            : this(string.Empty, errors) { }
 
         /// <summary>
-        /// Adds a new error to the collection.
+        /// Adds a new error to the collection with the specified key and errors.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="errors"></param>
+        public void Add(string key, params string[] errors)
+        {
+            if (key == null) // we allow empty key (i.e.. "")
+            {
+                throw new ArgumentException("Key cannot be null.", nameof(key));
+            }
+
+            if (errors == null)
+            {
+                throw new ArgumentException("Errors cannot be null.", nameof(errors));
+            }
+
+            if (!_resultErrors.TryGetValue(key, out var existingErrors))
+            {
+                _resultErrors.Add(key, errors.ToList());
+            }
+            else
+            {
+                foreach (var error in errors)
+                {
+                    existingErrors.Add(error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a new error to the collection with the specified key and errors.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="errors"></param>
+        public void Add(string key, IEnumerable<string> errors) =>
+            Add(key, errors.ToArray());
+
+        /// <summary>
+        /// Adds a new error to the collection with the specified key and error.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="error"></param>
+        public void Add(string key, string error) =>
+            Add(key, new[] { error });
+
+        /// <summary>
+        /// Adds a new error to the collection with the specified key.
         /// </summary>
         /// <param name="error"></param>
-        public void Add(ResultError error) =>
-            _errors.Add(error);
+        public void Add(string error) =>
+            Add(string.Empty, error);
+
+        /// <summary>
+        /// Adds a range of errors to the collection.
+        /// </summary>
+        /// <param name="errors"></param>
+        public void Add(params string[] errors) =>
+            Add(string.Empty, errors);
 
         /// <summary>
         /// Returns the enumerator for the collection.
         /// </summary>
         /// <returns></returns>
         public IEnumerator<ResultError> GetEnumerator() =>
-            _errors.GetEnumerator();
+            _resultErrors.Select(x => new ResultError(x.Key, x.Value.ToArray())).GetEnumerator();
 
         /// <summary>
         /// Returns the enumerator for the collection.
@@ -119,9 +144,20 @@ namespace Danom
         /// <returns></returns>
         public override string ToString()
         {
-            var errors = string.Join(", ", _errors);
-            return string.Concat("[ ", errors, " ]");
+            if (_resultErrors.Count == 0)
+            {
+                return "[]";
+            }
+
+            var formattedErrors = _resultErrors.Select(x =>
+                string.IsNullOrWhiteSpace(x.Key)
+                    ? string.Join(", ", x.Value)
+                    : string.Concat(x.Key, " - ", string.Join(", ", x.Value))
+            );
+
+            return string.Concat("[ ", string.Join("; ", formattedErrors), " ]");
         }
+
     }
 
     /// <summary>
@@ -137,6 +173,11 @@ namespace Danom
         /// <param name="errors"></param>
         public ResultError(string key, params string[] errors)
         {
+            if (key == null) // we allow empty key (i.e.. "")
+            {
+                throw new ArgumentException("Key cannot be null.", nameof(key));
+            }
+
             Key = key;
             Errors = errors;
         }
