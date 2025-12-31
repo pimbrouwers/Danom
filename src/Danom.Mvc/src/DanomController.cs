449e40d1b1dc02d593a7a7313201e9d94cd6ecd1
Pim Brouwers <pimnation@gmail.com>
2025-12-31 16:39:30 -0500

    streamline DanomController methods for improved readability and maintainability

--- src/Danom.Mvc/src/DanomController.cs
+++ src/Danom.Mvc/src/DanomController.cs
@@ -5,8 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 /// <summary>
 /// A controller that provides helper methods for working with Danom's monad types.
 /// </summary>
-public class DanomController
-    : Controller {
+public class DanomController : Controller {
     /// <summary>
     /// An action result that returns a 404 Not Found response if the option is
     /// None, otherwise display the specified view with the option value.
@@ -21,28 +20,12 @@ public class DanomController
         Option<T> option,
         string? viewName = null,
         Func<IActionResult>? noneAction = null,
-        ResultErrors? errors = null) {
-        if (errors is not null) {
-            ModelState.AddResultErrors(errors);
-        }
-
-        return option.Match(
-            some: x => {
-                if (viewName is not null) {
-                    return View(viewName, x);
-                }
-                else {
-                    return View(x);
-                }
-            },
-            none: () => {
-                if (noneAction is not null) {
-                    return noneAction();
-                }
-
-                return NotFound();
-            });
-    }
+        ResultErrors? errors = null) =>
+        this.ViewOption(
+            option: option,
+            viewName: viewName,
+            noneAction: noneAction,
+            errors: errors);
 
     /// <summary>
     /// Renders the specified view with the specified model, and add the
@@ -55,22 +38,11 @@ public class DanomController
     public IActionResult ViewResultErrors(
         ResultErrors errors,
         string? viewName = null,
-        object? model = null) {
-        ModelState.AddResultErrors(errors);
-
-        if (viewName is not null && model is not null) {
-            return View(viewName, model);
-        }
-        else if (viewName is not null) {
-            return View(viewName);
-        }
-        else if (model is not null) {
-            return View(model);
-        }
-        else {
-            return View();
-        }
-    }
+        object? model = null) =>
+        this.ViewResultErrors(
+            errors: errors,
+            viewName: viewName,
+            model: model);
 
     /// <summary>
     /// An action result that renders the specified view with the specified model
@@ -88,22 +60,11 @@ public class DanomController
         Func<TError, IActionResult> errorAction,
         string? viewName = null,
         object? model = null) =>
-        result.Match(
-            ok: x => {
-                if (viewName is not null && model is not null) {
-                    return View(viewName, model);
-                }
-                else if (viewName is not null) {
-                    return View(viewName, x);
-                }
-                else if (model is not null) {
-                    return View(model);
-                }
-                else {
-                    return View(x);
-                }
-            },
-            error: errorAction);
+        this.ViewResult(
+            result: result,
+            errorAction: errorAction,
+            viewName: viewName,
+            model: model);
 
     /// <summary>
     /// An action result that renders the specified view with the specified model
@@ -118,10 +79,139 @@ public class DanomController
         Result<T, ResultErrors> result,
         string? viewName = null,
         object? model = null) =>
-        ViewResult(
+        this.ViewResult(
+            result: result,
+            viewName: viewName,
+            model: model);
+}
+
+/// <summary>
+/// Danom extension methods for <see cref="Controller" />.
+/// </summary>
+public static class DanomControllerExtensions {
+    /// <summary>
+    /// An action result that returns a 404 Not Found response if the option is
+    /// None, otherwise display the specified view with the option value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="controller"></param>
+    /// <param name="option"></param>
+    /// <param name="viewName"></param>
+    /// <param name="noneAction"></param>
+    /// <param name="errors"></param>
+    /// <returns></returns>
+    public static IActionResult ViewOption<T>(
+        this Controller controller,
+        Option<T> option,
+        string? viewName = null,
+        Func<IActionResult>? noneAction = null,
+        ResultErrors? errors = null) {
+        if (errors is not null) {
+            controller.ModelState.AddResultErrors(errors);
+        }
+
+        return option.Match(
+            some: x => {
+                if (viewName is not null) {
+                    return controller.View(viewName, x);
+                }
+                else {
+                    return controller.View(x);
+                }
+            },
+            none: () => {
+                if (noneAction is not null) {
+                    return noneAction();
+                }
+
+                return controller.NotFound();
+            });
+    }
+
+    /// <summary>
+    /// Renders the specified view with the specified model, and add the
+    /// provided errors to the model state.
+    /// </summary>
+    /// <param name="controller"></param>
+    /// <param name="errors"></param>
+    /// <param name="viewName"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public static IActionResult ViewResultErrors(
+        this Controller controller,
+        ResultErrors errors,
+        string? viewName = null,
+        object? model = null) {
+        controller.ModelState.AddResultErrors(errors);
+
+        if (viewName is not null && model is not null) {
+            return controller.View(viewName, model);
+        }
+        else if (viewName is not null) {
+            return controller.View(viewName);
+        }
+        else if (model is not null) {
+            return controller.View(model);
+        }
+        else {
+            return controller.View();
+        }
+    }
+
+    /// <summary>
+    /// An action result that renders the specified view with the specified model
+    /// if the result is Ok, otherwise execute the specified error action.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TError"></typeparam>
+    /// <param name="controller"></param>
+    /// <param name="result"></param>
+    /// <param name="errorAction"></param>
+    /// <param name="viewName"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public static IActionResult ViewResult<T, TError>(
+        this Controller controller,
+        Result<T, TError> result,
+        Func<TError, IActionResult> errorAction,
+        string? viewName = null,
+        object? model = null) =>
+        result.Match(
+            ok: x => {
+                if (viewName is not null && model is not null) {
+                    return controller.View(viewName, model);
+                }
+                else if (viewName is not null) {
+                    return controller.View(viewName, x);
+                }
+                else if (model is not null) {
+                    return controller.View(model);
+                }
+                else {
+                    return controller.View(x);
+                }
+            },
+            error: errorAction);
+
+    /// <summary>
+    /// An action result that renders the specified view with the specified model
+    /// if the result is Ok, otherwise display the errors in the model state.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="controller"></param>
+    /// <param name="result"></param>
+    /// <param name="viewName"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public static IActionResult ViewResult<T>(
+        this Controller controller,
+        Result<T, ResultErrors> result,
+        string? viewName = null,
+        object? model = null) =>
+        controller.ViewResult(
             result: result,
             viewName: viewName,
             model: model,
-            errorAction: e => ViewResultErrors(e, viewName, model));
+            errorAction: e => controller.ViewResultErrors(e, viewName, model));
 
 }
